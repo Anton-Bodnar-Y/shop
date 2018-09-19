8eea00489c41edc9126133440b011e762dc974c0
bodnar <anton.bodnar@nixsolutions.com>
2018-09-20 00:22:29 +0300

    'created-migrations'

--- database/migrations/2018_09_11_073627_create_products_table.php
+++ database/migrations/2018_09_11_073627_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('title');
+            $table->integer('category_id')->unsigned();
+            $table->integer('manufacturer_id')->unsigned();
+            $table->string('image');
             $table->string('code');
-            $table->integer('status')->unsigned();
-            $table->rememberToken();
+            $table->decimal('price', 8, 2);
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
---
+++ database/migrations/2018_09_11_081516_create_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('customer_id')->unsigned();
+            $table->integer('address_id')->unsigned();
+            $table->decimal('total_price', 8, 2);
+            $table->string('delivery_service');
+            $table->string('secession_delivery');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
